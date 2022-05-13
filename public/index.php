162b31380bbc8d17506b39d25d942dc10901b50a
Szymon <szymonalaszyszka@gmail.com>
2022-05-13 09:56:21 +0200

    repo init

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="styles.css" rel="stylesheet">
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body class="bg-gray-200">
+        <div class=" bg-white w-5/6 m-auto h-64 mt-44 flex justify-center ">
+            <div class=" w-20 h-20 bg-purple-700 rounded-t-full mx-auto self-end">
+
+            <img src="img/Vector.svg" alt="profile" class="ml-2 mt-[3px]">
+
+            </div>
+        </div>
+    </body>
+</html>
